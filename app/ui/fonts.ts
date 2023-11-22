727f85b129f034a4e9d3c6865fd922f9e6b638c7
danielscrypt <d.n9204@gmail.com>
2023-11-22 10:13:30 +0200

    update

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Indie_Flower, Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400' , '700'] });
+export const indieFlower = Indie_Flower({ subsets: ['latin'], weight: ['400'] });
